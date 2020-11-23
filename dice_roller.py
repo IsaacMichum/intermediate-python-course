85003fa45c3991b66e1eb96b309df7f59e2593e0
IsaacMichum <73753832+IsaacMichum@users.noreply.github.com>
2020-11-23 17:48:08 -0500

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,11 @@
 def main():
-
   import random
 
 
   roll = random.randint(1,6)
-  print(f 'You Rolled A {roll} !')
+  print(f'You Rolled A {roll}!')
 
   print('You rolled a die')
 
-if __name__== "__main__":
+  if __name__== "__main__":
   main()
